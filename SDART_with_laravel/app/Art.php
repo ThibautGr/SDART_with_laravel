88422e822b652039860e6215bd8a6ffe6f571e9f
Franck Gerdolle <franck.gerdolle@gmail.com>
2020-02-28 14:22:22 +0100

    view typeartglobal
    in work

--- SDART_with_laravel/app/Art.php
+++ SDART_with_laravel/app/Art.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Art extends Model
 {
     protected $table='art';
+    protected $table='art';
 
     public function typeart(){
         return $this->belongsTo('App\Typeart');
--- SDART_with_laravel/resources/views/page/typeartglobal.blade.php
+++ SDART_with_laravel/resources/views/page/typeartglobal.blade.php
@@ -9,5 +9,44 @@
                 </tr>   
                 @endforeach
             </table>
-
+<div class="container" > 
+    
+    <h1> Art {{$tart->id}}{{$tart->name}} </h1>
+          
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    <img src="..." class="card-img-top" alt="...">
+    </div>
+    <br>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    <img src="..." class="card-img-top" alt="...">
+    </div>
+    <br>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    <img src="..." class="card-img-top" alt="...">
+    </div>
+    <br>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    <img src="..." class="card-img-top" alt="...">
+    </div>
+</div>
 @endsection
